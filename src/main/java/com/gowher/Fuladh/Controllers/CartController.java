729bb7f0981afc842052faae3c819bdb70225bc9
GowherAhanger <gowherrashid22@gmail.com>
2025-06-21 12:18:35 +0530

    bug fixes after sudden rollback

--- src/main/java/com/gowher/Fuladh/Controllers/CartController.java
+++ src/main/java/com/gowher/Fuladh/Controllers/CartController.java
@@ -2,13 +2,12 @@ package com.gowher.Fuladh.Controllers;
 
 import java.util.List;
 
+import com.gowher.Fuladh.Models.CartItem;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.gowher.Fuladh.Models.Cart;
 import com.gowher.Fuladh.Services.CartService;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/cart")
@@ -20,5 +19,12 @@ public class CartController {
     public List<Cart> getCartItems() {
         return cartService.getCartItems();
     }
-
+    @PostMapping("/addToCart")
+    public String addToCart(
+            @RequestBody CartItem cartItem,
+            @RequestParam int userId,
+            @RequestParam int productId) {
+        return cartService.addProductToCart(cartItem, userId, productId);
+    }
 }
+
--- src/main/java/com/gowher/Fuladh/Repositories/CartRepo.java
+++ src/main/java/com/gowher/Fuladh/Repositories/CartRepo.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CartRepo extends JpaRepository<Cart, Integer> {
 
+    Cart findByUserId(int userId);
 }
